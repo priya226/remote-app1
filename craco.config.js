524959e7d569063ea7a02e815c8f47e06e093bc4
priya226 <pr35@iitbbs.ac.in>
2024-10-28 17:17:21 +0530

    webconfiig.js

--- craco.config.js
+++ craco.config.js
@@ -2,27 +2,29 @@ const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.plugins.push(
-        new ModuleFederationPlugin({
-          name: "app1", 
-          filename: "remoteEntry.js",
-          exposes: {
-            // Expose your component
-            "./App": "./src/App", // Adjust the path based on your folder structure
-          },
-          remotes: {
-            app2: "app2@http://localhost:3002/remoteEntry.js",
-          },
-          shared: {
-            react: { singleton: true, eager: true, requiredVersion: '^18.3.1' },
-            "react-dom": { singleton: true, eager: true, requiredVersion: '^18.3.1' },
-          },
-          
-        })
-      );
-
-      return webpackConfig;
+    configure: {
+      output: {
+        publicPath: "auto",
+      },
     },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "app1", 
+        filename: "remoteEntry.js",
+        exposes: {
+          // Expose your component
+          "./App": "./src/App", // Adjust the path based on your folder structure
+        },
+        remotes: {
+          app2: "app2@http://localhost:3002/remoteEntry.js",
+        },
+        shared: {
+          react: { singleton: true },
+          "react-dom": { singleton: true },
+        },
+        
+      })
+      ],
   },
 };
+
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-// const RemoteButton = React.lazy(() => import("app2/Button"));
+const RemoteButton = React.lazy(() => import("app2/Button"));
 
 function App() {
   return (
     <div>
       <h1>Welcome to App1</h1>
-      {/* <React.Suspense fallback="Loading Button...">
+      <React.Suspense fallback="Loading Button...">
         <RemoteButton> </RemoteButton>
-      </React.Suspense> */}
+      </React.Suspense>
     </div>
   );
 }
